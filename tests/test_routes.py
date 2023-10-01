183aecc7d0dc72cd0c93fde0ba5e3b4f30d7debf
huytran230801 <huytran230801>
2023-10-01 11:15:51 -0400

    Fix lint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,6 +22,7 @@ BASE_URL = "/accounts"
 
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -37,7 +38,6 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
         talisman.force_https = False
-
 
     @classmethod
     def tearDownClass(cls):
@@ -74,10 +74,10 @@ class TestAccountService(TestCase):
             accounts.append(account)
         return accounts
 
+
     ######################################################################
     #  A C C O U N T   T E S T   C A S E S
     ######################################################################
-
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
